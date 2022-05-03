debf6d5253ff136c5d138d638280e943cb94ad67
PhilosopherProgrammer <nekarakube@gmail.com>
2022-05-03 22:03:52 +0900

    useReducer example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Counter from "./Hooks/Counter";
+import Counter2 from "./Hooks/Counter2";
 import Info from "./Hooks/Info";
 
 function App() {
@@ -12,10 +13,12 @@ function App() {
       <div style={{marginBottom: "10px"}}>
         <button onClick={() => setHook("useState")}>useState</button>
         <button onClick={() => setHook("useEffect")}>useEffect</button>
+        <button onClick={() => setHook("useReducer")}>useReducer</button>
       </div>
       <div>
         {hook === "useState" ? <Counter /> : null}
         {hook === "useEffect" ? <Info /> : null}
+        {hook === "useReducer" ? <Counter2 /> : null}
       </div>
     </div>
   );
